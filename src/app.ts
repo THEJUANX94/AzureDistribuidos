6a77755698ae2066941bbccd8aa0574343fcc332
THEJUANX94 <sebastianmn03@hotmail.com>
2022-10-08 16:13:10 -0500

    a

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from 'express'
 import cors from 'cors'
 import morgan from 'morgan'
+import sslRedirect from 'heroku-ssl-redirect';
 import studentsRoutes from './routes/Students.routes'
 import subjectsRoutes from './routes/Subjects.routes'
 
@@ -9,6 +10,7 @@ const app = express()
 
 app.use(morgan('dev'))
 app.use(cors())
+app.use(sslRedirect());
 app.use(express.json())
 
 app.use(studentsRoutes)
