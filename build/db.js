80a0b50edcf9eb3e1c627e0e87fe59288082d82e
THEJUANX94 <sebastianmn03@hotmail.com>
2022-10-08 15:40:29 -0500

    a

--- build/db.js
+++ build/db.js
@@ -5,20 +5,16 @@ const typeorm_1 = require("typeorm");
 const Students_1 = require("./Entities/Students");
 const Subjects_1 = require("./Entities/Subjects");
 exports.appDataSource = new typeorm_1.DataSource({
-    driver: {
-        type: "postgres",
-        host: "ec2-3-214-2-141.compute-1.amazonaws.com",
-        port: 5432,
-        username: "vatsniztbslyfb",
-        password: "d930b2a2e6b38b54ea3ef09b3099da223fe197f27dde238edcbffab859936411",
-        database: "d70eoltrth87l2",
-        synchronize: true,
-        logging: true,
-        extra: {
-            ssl: {
-                rejectUnauthorized: false
-            },
-        },
-        entities: [Students_1.Students, Subjects_1.Subject],
-    }
+    type: "postgres",
+    host: "ec2-3-214-2-141.compute-1.amazonaws.com",
+    port: 5432,
+    username: "vatsniztbslyfb",
+    password: "d930b2a2e6b38b54ea3ef09b3099da223fe197f27dde238edcbffab859936411",
+    database: "d70eoltrth87l2",
+    synchronize: true,
+    logging: true,
+    ssl: {
+        rejectUnauthorized: false
+    },
+    entities: [Students_1.Students, Subjects_1.Subject],
 });
