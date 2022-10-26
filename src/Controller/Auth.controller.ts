9c162b17fc42303b827c28cb59ebcd966a5c253c
THEJUANX94 <sebastianmn03@hotmail.com>
2022-10-25 22:14:56 -0500

    Creacion de la autentificacion por medio de JWT

---
+++ src/Controller/Auth.controller.ts
@@ -0,0 +1,42 @@
+import { Request, Response } from "express";
+import {Authentication} from '../Entities/Auth';
+import jwt  from "jsonwebtoken";
+
+export const signup = async (req: Request, res: Response) =>{
+    const {Mail, Password} = req.body;
+    const user = new Authentication();
+    user.Mail = Mail;
+    if(req.body.passwordUser){
+        user.Password = await user.encryptPassword(Password);
+    }
+
+    await user.save();
+    const token: string = jwt.sign({Mail: user.Mail}, process.env.TOKEN_SECRET || 'tokentest');
+    res.header('auth_token', token).json(user);
+}
+
+export const signin = async(req: Request, res: Response) =>{
+    try {
+        if(req.body.mailUser && req.body.passwordUser){
+            const user = await Authentication.findOneBy({Mail: req.body.Mail});
+            if(!user) return res.status(400).json('Email or password is wrong');
+            const correctPassword: boolean = await user.validatePassword(req.body.passwordUser);
+            const token: string = jwt.sign({Mail: user.Mail}, process.env.TOKEN_SECRET || 'tokentest', {
+                expiresIn: 60* 60* 24
+            });
+            res.header('auth_token', token).json(user);
+        }
+       
+    } catch (error) {
+        if(error instanceof Error){
+            return res.status(400).json({message: error.message});
+        }
+        
+    }
+}
+
+export const profile = async (req: Request, res: Response) =>{
+    const user = await Authentication.findOneBy({Mail: req.body.Mail});
+    if(!user) return res.status(400).json('No user found');
+    res.json(user);
+}
---
+++ src/Entities/Auth.ts
@@ -0,0 +1,24 @@
+import { Column, Entity, BaseEntity, PrimaryColumn } from 'typeorm'
+import bcrypt from "bcryptjs";
+
+
+@Entity('Authentication')
+export class Authentication extends BaseEntity {
+
+    @PrimaryColumn()
+    Mail: string
+
+    @Column()
+    Password: string
+
+
+    public encryptPassword = async (password: string): Promise<string> => {
+        const salt = await bcrypt.genSalt(10);
+        return bcrypt.hash(password, salt);
+    };
+
+    public validatePassword = async (password: string): Promise<boolean> => {
+        return await bcrypt.compare(password, this.Password);
+    }
+
+}
