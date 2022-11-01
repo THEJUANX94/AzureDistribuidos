8da63971a78019022842b7298789e40533108df2
THEJUANX94 <sebastianmn03@hotmail.com>
2022-11-01 15:57:34 -0500

    Eliminacion de modulos innecesarios

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,6 @@ import express from 'express'
 import cors from 'cors'
 import morgan from 'morgan'
 import path from 'path';
-import sslRedirect from 'heroku-ssl-redirect';
 import studentsRoutes from './routes/Students.routes'
 import subjectsRoutes from './routes/Subjects.routes'
 import registrationRoutes from './routes/Registration.routes'
@@ -13,7 +12,6 @@ const app = express()
 
 app.use(morgan('dev'))
 app.use(cors())
-app.use(sslRedirect());
 app.use(express.json())
 
 app.use(studentsRoutes)
